c7c760d8cde2a09226a2b23727bccc36ffc915d0
Stephen Fay <dcxstephen@live.co.uk>
2022-04-28 15:12:55 +0100

    working on wavelet bank

--- src/sm_make_wavelet_bank.py
+++ src/sm_make_wavelet_bank.py
@@ -1,41 +1,62 @@
-# sm_getPowerPerChannel.m is the MatLab equivalent
+# This file is based off of sm_getPowerPerChannel.m 
+
+import toml
+import warnings
 
 
-config = load_toml()    # load the toml config file
-RAW_DATA_PATH = config.fileio.RAW_DATA_PATH # somthing like this
-WAVELET_BINARIES_PATH = config.fileio.WAVELET_BINARIES_PATH
+def _load_fileio_and_data_ops(options_path="../Options.toml"):
+    warnings.warning("Change this relative path once package is configured properly. We need a more reliable way of accessing the options.toml config file")
+    with open(options_path,"r") as f:
+        ops_string = f.read()
+    ops = toml.loads(ops_string)
+    fileio = ops["fileio"]
+    data_ops = ops["params"]["data"]
+    return fileio,data_ops
 
+# def get_wavelet_bank(
+#         path_to_edf_file,
+#         n_chan,
+#         now,
+#         list_,
+#         all,
+#         the,
+#         options,
+#         its,
+#         unfortunately,
+#         the,
+#         least,
+#         of_evils): # get power per channel
+#     return
 
-def get_wavelet_bank(
-        path_to_edf_file,
-        n_chan,
-        now,
-        list_,
-        all,
-        the,
-        options,
-        its,
-        unfortunately,
-        the,
-        least,
-        of_evils): # get power per channel
-    return
+def wavelet_convolve(signal, fs, freq,...):
+    return # dummy
 
-def wavelet_convolve(signal, fs, freq):
-    return 
+def make_wavelet_bank(edf_fname,options_filepath):
+    """Computes and saves a wavelet decomposition of each channel. 
+    
+    Parameters
+    ----------
 
+    Returns nothing (look up how to express this in numpy doc syntax)
 
-def main():
-    # Find all raw.edf and raw.txt pairs inside RAW_DATA_PATH
-    # raw_data_files = [(patient_1_edf_path,patient_1_txt_path),(p2_edf,p2_txt),...]
+    """
+    assert edf_fname[-4:] == ".edf", f"Incorrect file format, expected .edf, got {edf_fname}"
+    assert options_path[-5:] == ".toml", f"Incorect file format, expected .toml extension, got {options_path}"
+    fileio,data_ops = load_fileio_and_data_ops(options_path)
 
-    # For each file, get the power on each channel
-    kwargs = {"n_chan":xyz,"abc":config.params.data.etc} # varargin
-    for edf,path in raw_data_files:
-        get_wavelet_bank(edf,**kwargs)
+    # Unpack File IO constants
+    RAW_DATA_PATH = fileio["RAW_DATA_PATH"]
+    WAVELET_BINARIES_PATH = fileio["WAVELET_BINARIES_PATH"]
 
+    # Unpack Data config constants
+    FS = data_ops["FS"]
+    NUM_FREQ = data_ops["NUM_FREQ"]
+    LOW_FREQ = data_ops["LOW_FREQ"]
+    HIGH_FREQ = data_ops["HIGH_FREQ"]
+    SPACING = data_ops["SPACING"]
+    ZSCORE_POWER = data_ops["ZSCORE_POWER"]
+    SCALE_PHASE = data_ops["SCALE_PHASE"]
+    SCALE_POWER = data_ops["SCALE_POWER"]
 
-
-
-
+    return # dummy 
 
