b8997ffebc980c741bb22edf0f76ac73adca9ae3
Stephen Fay <dcxstephen@live.co.uk>
2022-04-28 13:38:32 +0100

    metadata parser written v1

--- src/file_io/metadata_io.py
+++ src/file_io/metadata_io.py
@@ -1,5 +1,34 @@
-import pynapple as nap
+import numpy as np
 
-# parses 
-def seizure_txt_parser(file_path:str) -> (dict, nap.Tsd):
-    return 
+frontmatter = {}
+seizure_intervals = {}
+
+with open("metadata.txt","r") as f:
+    def fmt(line): return "".join(line.split("\n")).split("\t")
+
+    # read the frontmatter into a dictionary
+    line = fmt(f.readline())
+    while line != [""]:
+        assert len(line) == 2 , "Frontmatter format error, must contain key-value pairs seperated by tabs."
+        key,value = line
+        frontmatter[key] = value
+        line = fmt(f.readline())
+
+    # skip lines whitespace lines
+    while line == [""]:
+        line = fmt(f.readline())
+
+    # read seizure interval data into list
+    columns = line # Number, Start Time, End Time, Time From Start, Channel, Annotation
+    len_col = len(columns)
+    line = fmt(f.readline())
+    table = []
+    while line !=[""]:
+        assert len(line) == len_col , "Length of row data incompatible with number of columns."
+        table.append(line)
+        line = fmt(f.readline())
+    print("Finished reading annotation file.")
+
+    # transpose and put into columns dictionary
+    table_transpose = np.asarray(table).T
+    seizure_intervals = {colname:data for colname,data in zip(columns,table_transpose)}
