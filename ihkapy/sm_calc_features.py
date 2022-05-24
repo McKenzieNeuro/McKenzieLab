1fcf48f29dfd3d37ff58363b8d575cc9fa518dae
Stephen Fay <dcxstephen@live.co.uk>
2022-05-24 16:37:02 +0200

    temporary features written

--- ihkapy/sm_calc_features.py
+++ ihkapy/sm_calc_features.py
@@ -1,4 +1,8 @@
 import numpy as np
+from ihkapy.fileio.utils import get_all_valid_session_basenames,check_session_basenames_are_valid
+from ihkapy.fileio.metadata_io import get_seizure_start_end_times
+from scipy.signal import coherence
+
 
 """
 MatLab code is structures as follows:
@@ -187,11 +191,12 @@ def _get_total_session_time(filepath,fs=2000,num_bin_chan=41,precision="int16"):
     return total_duration_in_seconds
 
 
+# TODO: once completed, refactor this method
 def calc_features(
-        binary_basename_list : list,
         fileio : dict,
         data_ops : dict,
         feature_ops : dict,
+        session_basenames_list : list or str = "all",
         ) -> np.ndarray:
     """Compute all features in all channels for multiple sessions.
 
@@ -201,6 +206,9 @@ def calc_features(
     
     Parameters
     ----------
+    session_basenames_list : list or str
+        If the list is empty, it will default to all valid edf files in the 
+        directory. 
 
     Returns
     -------
@@ -216,29 +224,92 @@ def calc_features(
     # feature_functions = # TODO: this is a list of functions
     # features = # TODO: 2d np array shape = (n_windows,n_feats_per_window)
 
-    # Unpack relevant parameters
+    # Unpack fileio params
+    RAW_DATA_PATH = fileio["RAW_DATA_PATH"]
+
+    # Unpack data params
+    SCALE_PHASE     = feature_ops["SCALE_PHASE"]
+    SCALE_POWER     = feature_ops["SCALE_POWER"]
+
+    # Unpack relevant parameters, params.feature in Options.toml
     BIN_NAMES       = feature_ops["BIN_NAMES"]
     PREICTAL_BINS   = feature_ops["PREICTAL"]["BINS"] # in (negative) seconds
     POSTICTAL_DELAY = feature_ops["POSTICTAL"]["DELAY"] # in seconds
     PREICTAL_PCT    = feature_ops["PREICTAL"]["PCT"]
-    # unpack other stuff
-    bins = {} # TODO: define this, keys=bin_names, values=relative_intervals
-              # relative interval is a tuple (rel start, rel end)
-              # should be negative for pre-ictal
+    INTRAICTAL_PCT  = feature_ops["INTRAICTAL"]["PCT"]
+    POSTICTAL_PCT   = feature_ops["POSTICTAL"]["PCT"]
+    PCT = PREICTAL_PCT + [INTRAICTAL_PCT] + [POSTICTAL_PCT] # Concatenation
+    PCT_DIC = {b:pct for b,pct in zip(BIN_NAMES,PCT)} # a handy pct dictionary 
+    DUR_FEAT        = feature_ops["DUR_FEAT"]
+    amp_idx         = feature_ops["AMP_IDX"]
+    amp_idx = np.array(amp_idx) * 2 + 1 
+    ph_idx          = feature_ops["PH_IDX"]
+    ph_idx  = np.array(ph_idx) *  2 + 2 
 
-    dataframe_init = {bn:[] for bn in BIN_NAMES}
-    dataframe_init["session_basename"] = []
-    # TODO: carry on working here
-    bins_data_all = {b:pd.DataFrame(dataframe) for b in BIN_NAMES} # TODO: turn list into pandas dataframe
 
-    for session_basename in binary_basename_list:
-        # TODO: implement get_seizure_start_end_times()
-        start_times,end_times = get_seizure_start_end_times(session_basename,fileio)
-        total_session_time = _get_total_session_time(basename,precision) # in seconds
+    ### BEGIN, TEMPORARY HACK
+    # TODO: implement this properly so that it uses Options.toml instead
+    # of hard coding so that user doesn't have to edit code. 
+    # The ideal situation is to have a codebase robust enough that, most
+    # of the time, the researcher doesn't have to worry about the code,
+    # and when they do, they only have to write a new feature function in
+    # the features module, and then add it's name to a list in the Options.toml
+    # config file, and thats all—the code should work. 
+
+    # Define all the feature functions
+    # Features on each channel
+    def mean_power(window) -> np.ndarray:
+        """Returns mean across all Power channels. 
+        Warning! Power channel indices hard-coded."""
+        return np.mean(window[:,np.arange(1,40,2)],axis=0)/SCALE_POWER
+    # Features on each pair of channels n*(n-1)/2
+    def cohere_two_windows(window1,window2) -> np.ndarray:
+        """Returns coherence across select power channels"""
+        w1,w2 = window1[amp_idx],window2[amp_idx]
+        coherence(w1, w2, fs=FS, window='hann', nperseg=256, noverlap=None, nfft=None, detrend='constant', axis=0)
+
+        return # Dummy TODO: complete this method and continue
+
+
+    
+    # This is a dictionary with key=feature_name, value=feature_function
+    features_one_channel = {
+            "mean_power":mean_power
+            }
+    features_two_channel = {
+            "coherence":cohere_two_windows
+            }
+    
+
+    ### END  , TEMPORARY HACK
+
+
+    if session_basenames_list == "all": 
+        session_basenames_list = get_all_valid_session_basenames(RAW_DATA_PATH)
+    elif type(session_basenames_list)==type([]):
+        assert check_session_basenames_are_valid(session_basenames_list,RAW_DATA_PATH)
+    else: raise Exception()
+        
+
+    # Init Pandas DataFrame
+    df_cols = {ft:[] for ft in FEATS}
+    df_cols["session_basename"] = []
+    df_cols["bin_name"] = [] # the name of the bin, one of BIN_NAMES
+    for ft in feature_list: df_cols[f"{ft}"] = [] # TODO, this line
+    df = pd.DataFrame(dataframe_init)
+
+    for session_basename in session_basenames_list:
+        # Retrieve seizure times from metadata
+        session_metadata_path = os.path.join(RAW_DATA_PATH, session_basename+".txt")
+        start_times,end_times = get_seizure_start_end_times(session_metadata_path)
+        total_session_time = _get_total_session_time(basename,precision) # in secs
         for start_time,end_time in zip(start_times,end_times):
-            # TODO: define absolute start and end intervals for each bin
-            # TODO: test _get_bin_absolute_intervals()
-            #       returns a dic with keys=BIN_NAMES,values=(strtbin,endbin) (in s)
+            # _get_bin_absolute_intervals() returns a dic with 
+            # keys=BIN_NAMES,values=(strtbin,endbin) (in s). 
+            # If the intervals of a bin are not valid, either because 
+            # they start before or end after a file or because they 
+            # overlap with another seizure: the value at bin_name 
+            # is None.
             bins_absolute_intervals = _get_bin_absolute_intervals(
                     seizure_start_time   = start_time,
                     seizure_end_time     = end_time,
@@ -249,13 +320,29 @@ def calc_features(
                     total_session_time   = total_session_time
                     )
         
-            for bin_name,interval in bins_absolute_intervals if interval is not None:
-                print("dummy print statement")
-                # TODO: get the windows xPctTims thingy
-                # TODO: compute all features for this interval
-                # TODO: add a row to the data[bin_name] pandas data-frame
-                #   row includes session_basename, the start times of the windows,
-                #   
+            for bin_name in BIN_NAMES:
+                interval = bins_absolute_intervals[bin_name]
+                pct      = PCT_DIC[bin_name]
+                if interval is not None:
+                    start_time,end_time = interval
+                    print("dummy print statement")
+                    window_starts = _get_x_pct_time_of_interval(
+                            start_time      = start_time,
+                            end_time        = end_time,
+                            window_length   = DUR_FEAT,
+                            pct             = pct
+                            )
+                    ### Pseudocode
+                    # For each start time
+                    # Init a list of windows
+                    # Load each channel into a window, add each window to the list
+                    # Compute all features
+                    # Add a row to our features data-frame
+
+                    # TODO: compute all features for each pct window in interval
+                    # TODO: add a row to the data[bin_name] pandas data-frame
+                    #   row includes session_basename, the start times of the windows
+                    #   see the jupyter notebook for inspiration on how to add row
 
     # Read data params
     # Do some checks on the data and metadata params
@@ -373,6 +460,10 @@ if __name__=="__main__":
 
     # Not every single edge-case is tested... (low priority TODO)
     print("Tests All Passed: _get_bin_absolute_intervals()")
+
+
+
+
 
 
 
