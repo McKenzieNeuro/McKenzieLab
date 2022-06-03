e896dedea22ac643ce7ec8b9f42d01ed5a77fb9f
Stephen Fay <dcxstephen@live.co.uk>
2022-06-03 16:55:37 +0200

    tested and debugged calc_features

--- ihkapy/sm_calc_features.py
+++ ihkapy/sm_calc_features.py
@@ -1,12 +1,13 @@
 import numpy as np
 import os
 from ihkapy.fileio.utils import get_all_valid_session_basenames,check_session_basenames_are_valid
-from ihkapy.fileio.binary_io import get_n_samples_from_dur_fs
+from ihkapy.fileio.binary_io import get_n_samples_from_dur_fs,load_binary_multiple_segments
 from ihkapy.fileio.metadata_io import get_seizure_start_end_times
 from scipy.signal import coherence
 import sm_features
 import warnings
 import pandas as pd
+from tqdm import tqdm
 
 
 # pure function
@@ -43,7 +44,7 @@ def _get_x_pct_time_of_interval(
     # List of all start times of max number of non-overlapping windows
     # that fit in the interval.
     window_start_times = np.linspace(start_time,end_time-window_length,n_windows)
-    if pct == 1.0: return all_window_start_times
+    if pct == 1.0: return window_start_times
     # Choose and sort a random sample according to pct
     n_select = int(np.ceil(n_windows * pct)) # num win to select
     window_start_times = np.random.choice(window_start_times,n_select,replace=False)
@@ -53,7 +54,7 @@ def _get_x_pct_time_of_interval(
 
 def get_feats(
         segment : np.ndarray or list,
-        featurelist : list,
+        features_list : list,
         data_ops : dict
         ) -> dict:
     """Computes all features specified in featurelist.
@@ -73,7 +74,7 @@ def get_feats(
         Is a list of windows ordered by the raw_chan index. Each window 
         corresponds to a dur_feat segment of data. 
         Shape = (n_raw_chan , n_samples , n_wavelet_chan)
-    featurelist : list
+    features_list : list
         A list of strings.
     data_ops : dict
         Dictionary containing metadata, as defined in Options.toml.
@@ -89,7 +90,7 @@ def get_feats(
             warnings.warn(f"{i} has not yet been implemented.")
     all_feats = {}
     if "mean_power" in features_list:
-        AMP_FREQ_IDX_ALL = data_ops[AMP_FREQ_IDX_ALL]
+        AMP_FREQ_IDX_ALL = data_ops["AMP_FREQ_IDX_ALL"]
         mp_feats = sm_features.mean_power(segment,AMP_FREQ_IDX_ALL)  
         all_feats.update(mp_feats) # add it to greater dictionary
     if "var" in features_list:
@@ -102,6 +103,8 @@ def get_feats(
                 fs = FS
                 )
         all_feats.update(coherence_feats)
+    # print()
+    # for i,j in all_feats.items(): print(f"{i}:{j}") # debug
     return all_feats
 
 # Helper for calc_features()
@@ -218,17 +221,20 @@ def _get_bin_absolute_intervals(
     return bin_intervals
 
 
-def _get_total_session_time(
-        binary_filepath,
+def _get_total_session_time_from_binary(
+        binary_filepaths:list,
         fs=2000,
         n_chan_binary=41,
         precision="int16"
         ) -> float:
     """Determines the total session time from binary files."""
-    fsize_bytes = os.path.getsize(binary_filepath)
+    fsize_bytes = os.path.getsize(binary_filepaths[0])
+    # Check they are all exactly the same size, they must be
+    for i in binary_filepaths[1:]: 
+        assert fsize_bytes == os.path.getsize(i), "Corrupt data, Raw channel binaries mismatched filesize"
     bytes_per_sample = np.dtype(precision).itemsize
     n_samples_per_chan = fsize_bytes / bytes_per_sample / n_chan_binary
-    assert n_samples_per_chan == int(n_samples_per_chan) , "Logic error, possibly n_chan_binary is incorrect: this the number of channels saved in the binary file."
+    assert n_samples_per_chan == int(n_samples_per_chan), "Logic error, possibly n_chan_binary is incorrect: this the number of channels saved in the binary file."
     total_duration_in_seconds = n_samples_per_chan / fs # total duration in seconds
     return total_duration_in_seconds
 
@@ -236,20 +242,21 @@ def _get_total_session_time(
 def _get_feats_df_column_names(
         features_list   : list,
         data_ops        : dict,
-        n_chan_raw      : int,
-        n_chan_binary   : int
         ):
     """Calls the featurizing method on dummy windows and uses the columnames returned."""
     N_CHAN_RAW      = data_ops["N_CHAN_RAW"]
     N_CHAN_BINARY   = data_ops["N_CHAN_BINARY"]
-    colnames = ["session_basename","time_bin"] # first two colnames by default
     # Rem: session_basename identifies the recording, time_bin is the class label
-    dummy_windows = np.zeros(N_CHAN_RAW,10000,N_CHAN_BINARY) # 10000 samples is a 5s window at 2000Hz
+    # Dummy windows random noise not cnst or div by zero error in coherence (psd=0)
+    dummy_windows = np.random.normal(0,1,(N_CHAN_RAW,10000,N_CHAN_BINARY)) # 10000 samples is a 5s window at 2000Hz
     dummy_features = get_feats(dummy_windows,features_list,data_ops)
-    for feat_name in dummy_features.keys():
-        colnames.append(feat_name)
+    colnames = ["session_basename","time_bin"] # first two colnames by default
+    colnames += [k for k in dummy_features.keys()] # concatenate
     return colnames
 
+def _get_match_basename_in_dir_paths(directory,basename):
+    """Returns list of all full paths of files starting with b in basenames, in directory."""
+    return [os.path.join(directory,i) for i in os.listdir(directory) if i[:len(basename)]==basename]
 
 # TODO: test and refactor this method
 def calc_features(
@@ -295,7 +302,12 @@ def calc_features(
     N_CHAN_RAW      = data_ops["N_CHAN_RAW"]
     N_CHAN_BINARY   = data_ops["N_CHAN_BINARY"]
     PRECISION       = data_ops["PRECISION"]
-
+    # TODO: clean up amp and phase indexing conventions
+    amp_idx         = data_ops["AMP_IDX"]
+    AMP_IDX = np.array(amp_idx) * 2 + 1 
+    ph_idx          = data_ops["PH_IDX"]
+    PH_IDX  = np.array(ph_idx)  * 2 + 2 
+ 
     # Unpack relevant parameters, params.feature in Options.toml
     BIN_NAMES       = feature_ops["BIN_NAMES"]
     PREICTAL_BINS   = feature_ops["PREICTAL"]["BINS"] # in (negative) seconds
@@ -307,11 +319,6 @@ def calc_features(
     PCT_DIC = {b:pct for b,pct in zip(BIN_NAMES,PCT)} # a handy pct dictionary 
     DUR_FEAT        = feature_ops["DUR_FEAT"]
     N_SAMPLES       = get_n_samples_from_dur_fs(DUR_FEAT,FS) # n samples per feature
-    # TODO: clean up amp and phase indexing conventions
-    amp_idx         = feature_ops["AMP_IDX"]
-    AMP_IDX = np.array(amp_idx) * 2 + 1 
-    ph_idx          = feature_ops["PH_IDX"]
-    PH_IDX  = np.array(ph_idx)  * 2 + 2 
     FEATURES   = feature_ops["FEATURES"]
 
     # Define / check list of sessions
@@ -319,26 +326,33 @@ def calc_features(
         session_basenames_list = get_all_valid_session_basenames(RAW_DATA_PATH)
     elif type(session_basenames_list)==type([]):
         assert len(session_basenames_list) > 0, "No session files provided."
+        print(f"\nsession basenames {session_basenames_list}") # debug, temp
+        print(f"raw data path {RAW_DATA_PATH}\n") # debug, temp
         assert check_session_basenames_are_valid(session_basenames_list,RAW_DATA_PATH)
     else: raise Exception()
         
 
     # Init Pandas DataFrame with right colnames
-    colnames = _get_feats_df_column_names(FEATURES)
+    colnames = _get_feats_df_column_names(FEATURES,data_ops)
     feats_df = pd.DataFrame({name:[] for name in colnames})
 
     # For each session
     for session_basename in session_basenames_list:
         # Retrieve seizure times from metadata
         session_metadata_path = os.path.join(RAW_DATA_PATH, session_basename+".txt")
-        # List of binary wavelet & raw channel file paths, ordered by ch-idx
-        # Session names, must match same naming convention as in 
-        # make_wavelet_bank ('fname_out')
         start_times,end_times = get_seizure_start_end_times(session_metadata_path)
-        total_session_time = _get_total_session_time(basename,precision=PRECISION) # in secs
+        # All of the session binaries
+        session_binary_paths = _get_match_basename_in_dir_paths(WAVELET_BINARIES_PATH,session_basename)
+        # Get total session time, assumes all binary's exact same length
+        total_session_time = _get_total_session_time_from_binary(
+                session_binary_paths, 
+                fs=FS,
+                n_chan_binary=N_CHAN_BINARY,
+                precision=PRECISION) # in secs
 
         # For each seizure in the session
-        for start_time,end_time in zip(start_times,end_times):
+        print(f"Computing features for session {session_basename}")
+        for start_time,end_time in tqdm(list(zip(start_times,end_times))):
             # _get_bin_absolute_intervals() returns a dic with 
             # keys=BIN_NAMES,values=(strtbin,endbin) (in s). 
             # If the intervals of a bin are not valid, either because 
@@ -346,15 +360,16 @@ def calc_features(
             # overlap with another seizure: the value at bin_name 
             # is None.
             bins_absolute_intervals = _get_bin_absolute_intervals(
-                    seizure_start_time   = start_time,
-                    seizure_end_time     = end_time,
-                    preictal_bins        = PREICTAL_BINS, 
-                    posictal_delay       = POSTICTAL_DELAY,
-                    all_start_times      = start_times,
-                    all_end_times        = end_times,
-                    total_session_time   = total_session_time
+                    start_seiz          = start_time,
+                    end_seiz            = end_time,
+                    preictal_bins       = PREICTAL_BINS, 
+                    postictal_delay     = POSTICTAL_DELAY,
+                    bin_names           = BIN_NAMES,
+                    all_start_times     = start_times,
+                    all_end_times       = end_times,
+                    total_session_time  = total_session_time
                     )
-        
+
             # For each time bin (=interval label) corresponding to this session
             for bin_name in BIN_NAMES:
                 interval = bins_absolute_intervals[bin_name] # interval in seconds
@@ -369,7 +384,6 @@ def calc_features(
                             window_length   = DUR_FEAT,
                             pct             = pct
                             )
-
                     # This will hold the segments for this bin
                     bin_segments = np.zeros((
                         N_CHAN_RAW,
@@ -379,10 +393,12 @@ def calc_features(
                         ))
                     for raw_ch_idx in range(N_CHAN_RAW):
                         # Is it dangerous to use such a strict file-naming convention?
-                        sess_bin_chan_raw_path = f"{basename}_ch_{str(raw_ch_idx).zfill(3)}.dat"
+                        # Yes, TODO: refactor all nameings of things that derive 
+                        # from basenames to utility method
+                        session_binary_chan_raw_path = os.path.join(WAVELET_BINARIES_PATH,f"{session_basename}_ch_{str(raw_ch_idx).zfill(3)}.dat")
                         # Load all segments from specific time bin and raw chan
                         ws = load_binary_multiple_segments(
-                                file_path       = session_file_path,
+                                file_path       = session_binary_chan_raw_path,
                                 n_chan          = N_CHAN_BINARY,
                                 sample_rate     = FS,
                                 offset_times    = segment_starts,
@@ -403,7 +419,7 @@ def calc_features(
                             "time_bin"          : bin_name
                             })
                         # Add the row to our pandas dataframe
-                        feats_df.loc[len(df.index)] = feats
+                        feats_df.loc[len(feats_df.index)] = feats
     return feats_df
 
 
@@ -481,12 +497,12 @@ if __name__=="__main__":
     with open("temp_test.dat","wb") as file:
         arr.tofile(file)
     fs,n_chan_binary = 2,4
-    total_duration_in_seconds = _get_total_session_time(
-            binary_filepath = "./temp_test.dat",
-            fs = fs,
-            n_chan_binary = n_chan_binary,
-            precision = "int16"
-            )
+    total_duration_in_seconds = _get_total_session_time_from_binary(
+        binary_filepaths = ["./temp_test.dat"],
+        fs=fs,
+        n_chan_binary=n_chan_binary,
+        precision="int16"
+        )
     os.remove("temp_test.dat") # delete the test file
     try: 
         assert total_duration_in_seconds == len(arr) // fs // n_chan_binary
@@ -494,16 +510,55 @@ if __name__=="__main__":
     except: 
         print("Failed Test _get_total_session_time()")
 
+    ### TEST _get_feats_df_column_names(), and get_feats() implicitly
+    features = ["mean_power","coherence"]
+    data_ops = {"FS":2000,"NUM_FREQ":20,"LOW_FREQ":0.5,"HIGH_FREQ":200,
+            "SPACING":"LOGARITHMIC","ZSCORE_POWER":True,"SCALE_PHASE":1000,
+            "SCALE_POWER":1000,"N_CHAN_RAW":4,"CH_PHASE_AMP":2,
+            "TS_FEATURES":["WAVELET_POWER","WAVELET_PHASE"],
+            "N_CHAN_BINARY":3,"PRECISION":"int16",
+            "AMP_IDX":[],#[14,15,16,17,18,19],
+            "PH_IDX":[0],#[0,1,2,3,4,5,6,7,8,9],
+            "AMP_FREQ_IDX_ALL":[1],#,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39],
+            "PH_FREQ_IDX_ALL":[2]}#,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40]}
+    colnames = _get_feats_df_column_names(features,data_ops)
+    print("\nTest, first eight column names are:")
+    for i in colnames[:8]: print(i)
+
+    ### TEST calc_features
+    fio_ops = {"RAW_DATA_PATH":"/Users/steve/Documents/code/unm/data/h24_data/raw",
+            "WAVELET_BINARIES_PATH":"/Users/steve/Documents/code/unm/data/h24_data/binaries"}
+    # data_ops, use same dict as in above get_feats() test, but update
+    # it to include all amplitude and phase indices
+    data_ops["N_CHAN_BINARY"] = 41
+    data_ops["AMP_IDX"] = [14,15,16,17,18,19]
+    data_ops["PH_IDX"] = [0,1,2,3,4,5,6,7,8,9]
+    data_ops["AMP_FREQ_IDX_ALL"] = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
+    data_ops["PH_FREQ_IDX_ALL"] = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40]
+    feature_ops = {"DUR_FEAT":5,"N_PREICTAL_BINS":4,
+            "PREICTAL":{"BINS":[10800,3600,600,10],"PCT":[0.05,0.05,0.2,1.0]},
+            "INTRAICTAL":{"PCT":1.0},
+            "POSTICTAL":{"DELAY":600,"PCT":0.2},
+            "BIN_NAMES":["pre_ictal_bin1","pre_ictal_bin2","pre_ictal_bin3",
+                "pre_ictal_bin4","intra_ictal_bin","post_ictal_bin"],
+            "FEATURES":["mean_power","var","coherence"]}
+    session_basename_list = ["AC75a-5 DOB 072519_TS_2020-03-23_17_30_04"]
+    feat_df = calc_features(fio_ops,data_ops,feature_ops,session_basename_list)
+    # serialize feat dataframe to look at it in jupyter notebook
+    saveas = "feat_df.pkl"
+    print(f"Pickling features {saveas}")
+    feat_df.to_pickle(saveas)
+    print("Passed test calc_features()")
+
+
     # TODO
     """
     unit tests to implement:
 
-    _get_feats_df_column_names
-    get_feats
     calc_features
     """
 
-    print("Tests All Passed: _get_bin_absolute_intervals()")
+    print("\nTests All Passed: _get_bin_absolute_intervals()")
 
 
 
