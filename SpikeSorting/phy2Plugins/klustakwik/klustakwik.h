b5541109b035cb517f1294a30776c00279d0a79a
McKenzieNeuro <79343011+McKenzieNeuro@users.noreply.github.com>
2023-02-14 13:00:28 -0700

    custom phy2 plugins

---
+++ SpikeSorting/phy2Plugins/klustakwik/klustakwik.h
@@ -0,0 +1,217 @@
+/*
+ * Main header file
+ */
+
+#ifndef MASKED_KLUSTA_KWIK_2_H_
+#define MASKED_KLUSTA_KWIK_2_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdarg.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include "parameters.h"
+#include "log.h"
+#include "util.h"
+#include "numerics.h"
+#include "linalg.h"
+#include "memorytracking.h"
+
+using namespace std;
+
+class KK {
+public:
+    /////////////// CONSTRUCTORS ///////////////////////////////////////////////
+    // Construct from file
+    KK(char *FileBase, integer ElecNo, char *UseFeatures,
+            scalar PenaltyK, scalar PenaltyKLogN, integer PriorPoint);
+    // Construct from subset of existing KK object
+    void ConstructFrom(const KK &Source, const vector<integer> &Indices);
+	void ConstructFrom(const KK &Source);
+    KK(const KK &Source, const vector<integer> &Indices);
+    // Make an entire copy of existing KK object
+    KK(const KK &Source);
+	// Destructor
+	~KK();
+    /////////////// FUNCTIONS //////////////////////////////////////////////////
+	void MemoryCheck();
+    void AllocateArrays();
+    void Reindex();
+    // Random initial starting conditions functions
+    void StartingConditionsRandom();
+    void StartingConditionsFromMasks();
+    // Precomputation related functions
+    void DoInitialPrecomputations();
+    // Functions called by DoPrecomputations
+    void DoPrecomputations();
+    //SNK Precomputation for TrySplits to avoid changing NoiseMean and NoiseVariance and nMasked
+    void ComputeUnmasked();
+    void ComputeSortIndices();
+    void ComputeSortedUnmaskedChangePoints();
+    void ComputeNoiseMeansAndVariances();
+    void ComputeCorrectionTermsAndReplaceData();
+    void PointMaskDimension ();//SNK PointMaskDimension() computes the sum of the masks/float masks for each point
+    // Precomputations for cluster masks
+    void ComputeClusterMasks();
+    // Score and penalty functions
+    scalar ComputeScore();
+    scalar Penalty(integer n);
+    void ComputeClassPenalties();
+    // Main algorithm functions
+    void MStep();
+    void EStep();
+    void CStep(bool allow_assign_to_noise=true);
+    void ConsiderDeletion();
+    integer TrySplits();
+    scalar CEM(char *CluFile, integer recurse, integer InitRand, bool allow_assign_to_noise=true);
+    scalar Cluster(char *CluFile);
+    // IO related functions
+    void LoadData(char *FileBase, integer ElecNo, char *UseFeatures);
+    void LoadClu(char *StartCluFile);
+    void SaveOutput();
+    void SaveTempOutput();
+    void SaveSortedData();
+    void SaveSortedClu();
+    void SaveCovMeans();
+public:
+    /////////////// VARIABLES //////////////////////////////////////////////////
+	KK *KK_split, *K2_container; // used for splitting
+    integer nDims, nDims2; // nDims2 is nDims squared and the mean of the unmasked dimensions.
+    int nStartingClusters; // total # starting clusters, including clu 0, the noise cluster (int because read in from file)
+    integer nClustersAlive; // nClustersAlive is total number with points in, excluding noise cluster
+    integer nPoints;
+    integer priorPoint; // Prior for regularization NOTE: separate from global variabl PriorPoint (capitalization)
+    integer NoisePoint; // number of fake points always in noise cluster to ensure noise weight>0
+    integer FullStep; // Indicates that the next E-step should be a full step (no time saving)
+    scalar penaltyK, penaltyKLogN;
+
+#ifdef STORE_DATA_AS_INTEGER
+    vector<data_int> Data; // Data[p*nDims + d] = Input data for point p, dimension d
+#else
+    vector<scalar> Data; // Data[p*nDims + d] = Input data for point p, dimension d
+#endif
+    
+    // We sort the points into an order where the corresponding mask changes as
+    // infrequently as possible, this vector is used to store the sorted indices,
+    // see the function KK::ComputeSortIndices() in sortdata.cpp to see how this
+    // sorting works. In the case of the TrySplits() function this sorting needs
+    // to be recomputed when we change the underlying data.
+    vector<integer> SortedIndices;
+
+    vector<char> Masks; //SNK: Masks[p*nDims + d] = Input masks for point p, dimension d
+#ifdef COMPUTED_BINARY_MASK
+	inline char GetMasks(integer i)
+	{
+		if(!UseDistributional)
+			return Masks[i];
+#ifdef STORE_FLOAT_MASK_AS_CHAR
+		return CharFloatMasks[i]==(unsigned char)255;
+#else
+		return FloatMasks[i]==(scalar)1;
+#endif
+	}
+#else
+	inline char GetMasks(integer i) { return Masks[i]; }
+#endif
+
+#ifdef STORE_FLOAT_MASK_AS_CHAR
+	vector<unsigned char> CharFloatMasks; // float mask that is stored in a char to save RAM
+#else
+    vector<scalar> FloatMasks; // as above but for floating point masks
+#endif
+    // We store just the indices of the unmasked points in this sparse array
+    // structure. For point p, the segment Unmasked[UnmaskedInd[p]] to
+    // Unmasked[UnmaskedInd[p+1]] contains the indices i where Masks[i]==1.
+    // This allows for a nice contiguous piece of memory without a separate
+    // memory allocation for each point (and there can be many points). The
+    // precomputation is performed by the function ComputeUnmasked(), and is
+    // automatically called by LoadData() at the appropriate time, although
+    // in the case of TrySplits() it has to be called explicitly.
+    vector<integer> Unmasked;
+    vector<integer> UnmaskedInd;
+    // We also store a bool that tells us if the mask has changed when we go
+    // through the points in sorted order. Computed by function
+    // ComputeSortedUnmaskedChangePoints()
+    vector<integer> SortedMaskChange;
+    
+    vector<scalar> UnMaskDims; //SNK: Number of unmasked dimensions for each data point. In the Float masks case, the sum of the weights.
+    vector<integer> nClassMembers;
+    
+    vector<scalar> Weight; // Weight[c] = Class weight for class c
+    vector<scalar> Mean; // Mean[c*nDims + d] = cluster mean for cluster c in dimension d
+    vector<scalar> Cov; // Cov[c*nDims*nDims + i*nDims + j] = Covariance for cluster C, entry i,j
+                    // NB covariances are stored in upper triangle (j>=i)
+	vector<BlockPlusDiagonalMatrix> DynamicCov; // Covariance matrices, DynamicCov[cc] where cc is alive cluster index
+    vector<scalar> LogP; // LogP[p*MaxClusters + c] = minus log likelihood for point p in cluster c
+    vector<integer> Class; // Class[p] = best cluster for point p
+    vector<integer> OldClass; // Class[p] = previous cluster for point p
+    vector<integer> Class2; // Class[p] = second best cluster for point p
+    vector<integer> BestClass; // BestClass = best classification yet achieved
+    vector<integer> ClassAlive; // contains 1 if the class is still alive - otherwise 0
+    vector<integer> AliveIndex; // a list of the alive classes to iterate over
+
+    // Used for distributional optimisations
+    vector<scalar> ClusterMask;
+    vector< vector<integer> > ClusterUnmaskedFeatures;
+    vector< vector<integer> > ClusterMaskedFeatures;
+
+    // Used in EStep(), but this will probably change later
+    vector<scalar> AllVector2Mean;
+    // used in M step
+    vector<scalar> NoiseMean;
+    vector<scalar> NoiseVariance;
+    vector<integer> nMasked;
+    // used in distribution EM steps
+
+#ifdef COMPUTED_CORRECTION_TERM
+	inline scalar GetCorrectionTerm(integer p, integer i)
+	{
+            // scalar x = Data[p*nDims+i];
+#ifdef STORE_FLOAT_MASK_AS_CHAR
+            scalar w = CharFloatMasks[p*nDims+i]/(scalar)255.0;
+#else
+            scalar w = FloatMasks[p*nDims+i];
+#endif
+            scalar nu = NoiseMean[i];
+            scalar sigma2 = NoiseVariance[i];
+            //scalar y = w*x+(1-w)*nu;
+            //scalar z = w*x*x+(1-w)*(nu*nu+sigma2);
+			scalar y = GetData(p, i);
+			if(w==(scalar)0.0)
+			{
+				scalar z = nu*nu+sigma2;
+				return z-y*y;
+			} else
+			{
+				scalar x = (y-(1-w)*nu)/w;
+				scalar z = w*x*x+(1-w)*(nu*nu+sigma2);
+				return z-y*y;
+			}
+	};
+#else
+    vector<scalar> CorrectionTerm;
+#endif
+#ifdef STORE_DATA_AS_INTEGER
+	inline scalar GetData(integer p, integer i)
+	{
+		return data_scalar_from_int(Data[p*nDims+i]);
+	};
+#else
+	inline scalar GetData(integer p, integer i)
+	{
+		return Data[p*nDims+i];
+	};
+#endif
+
+    // used in ComputeScore and ConsiderDeletion
+    vector<scalar> ClassPenalty;
+    // debugging info
+    integer numiterations;
+	integer init_type;
+};
+
+#endif /* MASKED_KLUSTA_KWIK_2_H_ */
---
+++ SpikeSorting/phy2Plugins/klustakwik/parameters.h
@@ -0,0 +1,138 @@
+/*
+ * parameters.h
+ *
+ * This file is just used to declare the parameters that are shared across
+ * several files.
+ *
+ *  Created on: 11 Nov 2011
+ *      Author: dan
+ */
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include "numerics.h"
+
+enum type_t {FLOAT = 'f', INT = 'd',  BOOLEAN = 'b', STRING = 's'};
+
+#define FLOAT_PARAM(name) add_param(FLOAT, #name, &name)
+#define INT_PARAM(name) add_param(INT, #name, &name)
+#define BOOLEAN_PARAM(name) add_param(BOOLEAN, #name, &name)
+#define STRING_PARAM(name) add_param(STRING, #name, name)
+
+#define STRLEN 10000
+
+// This parameters table gives the command line parameters that are loaded into
+// global variables. The Param type is the macro used to load the parameter from
+// the command line, defined above. The Name is the variable name. The
+// Definition is the standard C++ definition of the variable without the default
+// value which is given by Default. This table is used in three places, below
+// the parameters are defined with an extern so that multiple files can access
+// the same global variables, e.g. extern integer ElecNo;. In parameters.cpp the
+// variables are instantiated and given their default values, e.g.
+// integer ElecNo = 1;. In parameters.cpp the parameters are read from the command
+// line, e.g. INT_PARAM(ElecNo).
+//  F(Documentation,
+//    Param type,    Name,                Definition,                Default)
+#define PARAMETERS_TABLE(F)                                                    \
+    F("Filename base, files are of the form FileBase.fet.ElecNo, etc.",        \
+      STRING_PARAM,  FileBase,            char FileBase[STRLEN],     "electrode")\
+    F("Electrode number, files are of the form FileBase.fet.ElecNo, etc.",     \
+      INT_PARAM,     ElecNo,              integer ElecNo,                1        )\
+    F("String of 0s and 1s indicating which features to use. "" indicates, `use all features' ",  \
+      STRING_PARAM,  UseFeatures,         char UseFeatures[STRLEN],  "")\
+    F("Drop the last N features in UseFeatures (works only when using "" for Usefeatures).",\
+      INT_PARAM,     DropLastNFeatures,       integer DropLastNFeatures,  0       )\
+    F("Use distributional EM steps",                                           \
+      BOOLEAN_PARAM, UseDistributional,   char UseDistributional,    1    )\
+    F("Run Mask starts with this many starting clusters",                      \
+      INT_PARAM,   MaskStarts,            integer MaskStarts,            500      )\
+    F("Minimum number of clusters to be used without splitting.",              \
+      INT_PARAM,     MinClusters,         integer MinClusters,           100      )\
+    F("Maximum number of clusters to be used without splitting.",              \
+      INT_PARAM,     MaxClusters,         integer MaxClusters,           110      )\
+    F("Maximum possible number of clusters to be used after splitting.",       \
+      INT_PARAM,     MaxPossibleClusters, integer MaxPossibleClusters,   1000     )\
+    F("Number of times to start count from each number of clusters.",          \
+      INT_PARAM,     nStarts,             integer nStarts,               1        )\
+    F("An intermediate cluster file to use as a starting point.",              \
+      STRING_PARAM,  StartCluFile,        char StartCluFile[STRLEN], ""       )\
+    F("Allow cluster splitting after this many iterations after initial split.",\
+      INT_PARAM,     SplitEvery,          integer SplitEvery,            40       )\
+    F("Allow first cluster splitting after this many iterations.",             \
+      INT_PARAM,     SplitFirst,          integer SplitFirst,            20       )\
+    F("Coefficient of num_params to use in penalty (1 for AIC).",              \
+      FLOAT_PARAM,   PenaltyK,            scalar PenaltyK,           0.0      )\
+    F("Coefficient of num_params*log(num_points)/2 to use in penalty (1 for BIC).",\
+      FLOAT_PARAM,   PenaltyKLogN,        scalar PenaltyKLogN,       1.0      )\
+    F("Do clustering on 1/Subset points, and then generalise to whole set.",   \
+      INT_PARAM,     Subset,              integer Subset,                1        )\
+    F("There is a full E step recomputation at least after this many iterations.",\
+      INT_PARAM,     FullStepEvery,       integer FullStepEvery,         20       )\
+    F("Maximum number of iterations.",                                         \
+      INT_PARAM,     MaxIter,             integer MaxIter,               10000    )\
+    F("Specify random seed for reproducible results, or leave for random.",    \
+      INT_PARAM,     RandomSeed,          integer RandomSeed,            1        )\
+    F("Whether or not to run in debug mode (prints lots of detail). 0 = None, 1 = Partial info, 2=Full Info",\
+      INT_PARAM, Debug,                   char Debug,                0        )\
+    F("Whether or not give spilt iteration info. 0 = Partial split info, 1 = Full split info",\
+      INT_PARAM, SplitInfo,                   char SplitInfo,        1        )\
+    F("Whether or not to print information as the program runs.",              \
+      INT_PARAM,     Verbose,             integer Verbose,               1        )\
+    F("???",                                                                   \
+      INT_PARAM,     DistDump,            integer DistDump,              0        )\
+    F("Points this far from best do not get an E step recomputation.",         \
+      FLOAT_PARAM,   DistThresh,          scalar DistThresh,         (scalar)log(1000.0))\
+    F("If this fraction of points changed class last time, do a full step.",   \
+      FLOAT_PARAM,   ChangedThresh,       scalar ChangedThresh,      .05      )\
+    F("Whether or not to save information to a log file.",                     \
+      BOOLEAN_PARAM, Log,                 char Log,                  1        )\
+    F("Whether or not to save temp clu files on every iteration instead of before every split.",\
+      BOOLEAN_PARAM, SaveTempCluEveryIter,   char SaveTempCluEveryIter,    0        )\
+    F("Log output to screen.",                                                 \
+      BOOLEAN_PARAM, Screen,              char Screen,               1        )\
+    F("Number of 'PriorPoints'",                                               \
+      INT_PARAM,   PriorPoint,            integer PriorPoint,            1        )\
+    F("Save FileBase.sorted.*.ElecNo data files (data in sorted order).",      \
+      BOOLEAN_PARAM, SaveSorted,          char SaveSorted,           false    )\
+    F("Save covariance and means",                                             \
+      BOOLEAN_PARAM, SaveCovarianceMeans, char SaveCovarianceMeans,  false    )\
+    F("Use mask based initial conditions",                                     \
+      BOOLEAN_PARAM, UseMaskedInitialConditions, char UseMaskedInitialConditions, false)\
+    F("Assign to first closest mask in mask based initial conditions",         \
+      BOOLEAN_PARAM, AssignToFirstClosestMask, char AssignToFirstClosestMask, false)\
+	F("Set RAM usage limit (0 for available memory, -1 for no limit)",         \
+      FLOAT_PARAM,     RamLimitGB,        scalar RamLimitGB,          0.0     )\
+	F("Always split bimodal clusters (can save time)",                         \
+      BOOLEAN_PARAM,   AlwaysSplitBimodal,char AlwaysSplitBimodal,    false   )\
+    F("Minimum number of points required for cluster mask.",                   \
+      FLOAT_PARAM,   PointsForClusterMask,  scalar PointsForClusterMask, 10 )\
+    F("Minimum point to cluster mask overlap.",                                \
+      FLOAT_PARAM,   MinMaskOverlap,      scalar MinMaskOverlap,      0.0     )\
+
+
+//TODO: Implement the two Debug modes, one less verbose than the other
+
+#define STRINGIFY(x) #x
+
+// These four lines define how to make the extern definitions, the full
+// instantiation of the variables, the inputting of the
+// parameters and the documentation (in parameters.cpp)
+#define EXTERN_PARAMETERS(DOC, TYPE, NAME, DEF, VAL) extern DEF;
+#define DEFINE_PARAMETERS(DOC, TYPE, NAME, DEF, VAL) DEF = VAL;
+#define INPUT_PARAMETERS(DOC, TYPE, NAME, DEF, VAL) TYPE(NAME);
+#define PRINT_PARAMETERS(DOC, TYPE, NAME, DEF, VAL) \
+    fprintf(stderr, "- " STRINGIFY(NAME) " = " STRINGIFY(VAL) "\n" \
+            "  " DOC "\n\n");
+
+PARAMETERS_TABLE(EXTERN_PARAMETERS)
+
+//////////// FUNCTIONS TO READ PARAMETERS FROM COMMAND LINE ////////////////////
+
+void add_param(integer t, char *name, void *addr);
+integer change_param(char *name, char *value);
+void init_params(integer argc, char **argv);
+void print_params(FILE *fp);
+void SetupParams(integer argc, char **argv);
+
+#endif /* PARAMETERS_H_ */
