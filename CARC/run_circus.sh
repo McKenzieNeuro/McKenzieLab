744b03ce15160ee9a788084a30b0f650d582191e
McKenzieNeuro <79343011+McKenzieNeuro@users.noreply.github.com>
2023-02-18 16:40:13 -0700

    fixed int to double conversion in sm_reClusterKlustakwik

--- CARC/run_circus.sh
+++ CARC/run_circus.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
-
-
-
-
-
-cd /carc/scratch/projects/mckenzie2016183/code
-
+cd /carc/scratch/projects/mckenzie2016183/code/matlab
 source activate circus
 
 
 
-spyking-circus $1-H $CARC_NODEFILE -c $SLURM_CPUS_PER_TASK 
-spyking-circus $1 -H $CARC_NODEFILE -m converting -c $SLURM_CPUS_PER_TASK 
+#spyking-circus $1 -H $CARC_NODEFILE -c $SLURM_CPUS_PER_TASK 
+#spyking-circus $1 -H $CARC_NODEFILE -m converting -c $SLURM_CPUS_PER_TASK 
 
-source deactivate
+#source deactivate
 
 matlab -singleCompThread -nodisplay  -nodesktop  -nojvm -r "sm_reClusterKlustakwikCarc('$1'); exit;"
 
---
+++ CARC/testMe.m
@@ -0,0 +1,7 @@
+function testMe(inputFile,CARC_NODEFILE,SLURM_CPUS_PER_TASK)
+int1 = num2str(round(100*rand(1)));
+CARC_NODEFILE
+SLURM_CPUS_PER_TASK
+save(['testMe' int1 '.mat'],'inputFile')
+
+end
