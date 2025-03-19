571f5368abc37c749d664e6ff7d4814dd1b09b6d
McKenzieNeuro <79343011+McKenzieNeuro@users.noreply.github.com>
2025-03-19 11:55:29 -0600

    fixed time shift

--- EEG_database/main.py
+++ EEG_database/main.py
@@ -4,12 +4,13 @@ from datetime import datetime, timedelta
 from pyedflib import highlevel
 import numpy as np
 
-def remove_sensitive_data_and_adjust_time(edf_file_path, output_file_path):
+
+def remove_sensitive_data_and_adjust_time(edf_file_path, output_file_path, time_shift):
     # Open the original EDF file
 
     signals, signal_headers, header = pyedflib.highlevel.read_edf(edf_file_path, digital=True)
     startdate = header['startdate']
-    startdate = startdate - timedelta(days=7)
+    startdate = startdate - timedelta(days=time_shift)
     header['annotations'] = []
     header['patientname'] = ''
     header['birthdate'] = ''
@@ -18,19 +19,20 @@ def remove_sensitive_data_and_adjust_time(edf_file_path, output_file_path):
     header['technician'] = ''
     header['recording_additional'] = ''
     header['startdate'] = startdate
-    #for i, signal in enumerate(signals):
+    # for i, signal in enumerate(signals):
 
     #    signal_min = signal_headers[i]['physical_min']
     #    signal_max = signal_headers[i]['physical_max']
 
-        # Clip the signal based on the individual physical_min and physical_max
+    # Clip the signal based on the individual physical_min and physical_max
     #    signals[i] = np.clip(signal, signal_min, signal_max)
 
     pyedflib.highlevel.write_edf(output_file_path, signals, signal_headers, header, digital=True)
 
     # to_remove = ['patientname', 'birthdate', 'admin_code','startdate','patientcode','technician','recording_additional']
     # new_values = ["xxx", '', 'test', startdate, '', '', '']
-    #highlevel.anonymize_edf(edf_file_path, to_remove=to_remove, new_values=new_values, verify=False, verbose=False)
+    # highlevel.anonymize_edf(edf_file_path, to_remove=to_remove, new_values=new_values, verify=False, verbose=False)
+
 
 def main():
     # Set up the argument parser
@@ -38,12 +40,12 @@ def main():
         description="Modify an EDF file by removing sensitive data and adjusting start time.")
     parser.add_argument('input_file', type=str, help="Path to the input EDF file.")
     parser.add_argument('output_file', type=str, help="Path to the output EDF file.")
-
+    parser.add_argument('time_shift', type=float, help="number of days to shift")
     # Parse the arguments
     args = parser.parse_args()
 
     # Call the function with the provided input and output file paths
-    remove_sensitive_data_and_adjust_time(args.input_file, args.output_file)
+    remove_sensitive_data_and_adjust_time(args.input_file, args.output_file, args.time_shift)
 
 
 if __name__ == '__main__':
